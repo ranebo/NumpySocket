7d533737263ba94d9c303dd3f9759a083ae2bada
Steeve Bjornson <info@sabjorn.net>
2018-02-10 08:38:10 +0000

    added beginning of kinect depth examples

---
+++ examples/kinect/README.md
@@ -0,0 +1,4 @@
+This is a modified version of this code: http://sphinx.mythic-beasts.com/~markt/RPi-openCV-Kinect.html
+Don't forget to add the modprobe to rc.local
+
+However, [this](https://docs.opencv.org/3.2.0/d7/d6f/tutorial_kinect_openni.html) appears to be a wholey OpenCV version. However, haven't gotten it to work.
---
+++ examples/kinect/demo_measure.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import freenect
+import cv2 as cv
+import numpy as np
+from numpysocket import NumpySocket
+
+host_ip = 'localhost'  # change me
+npSocket = NumpySocket()
+npSocket.startServer(host_ip, 9999)
+
+spread = 4
+midx = 320
+midy = 240
+sense_pt1 = (midx - spread, midy - spread)
+sense_pt2 = (midx + spread, midy + spread)
+sense_rect = (sense_pt1[0], sense_pt1[1], sense_pt2[0] - sense_pt1[0], sense_pt2[1] - sense_pt1[1])
+
+def show_depth():
+    global threshold
+    global current_depth
+
+    depth, timestamp = freenect.sync_get_depth()
+    viewable = depth #frame_convert.full_depth_cv (depth)
+    npSocket.sendNumpy(viewable)
+    #roi = cv.GetSubRect (frame_convert.raw_depth_cv (depth), sense_rect)
+    #pix = cv.Avg (roi)[0]
+    #(roimin, roimax, a, b) = cv.MinMaxLoc (roi)
+    #if roimax < 1090:
+    #  dist = 350.0 / (1091 - pix)
+    #  print "%f %i %i" % (dist, roimin, roimax)
+    #else:
+    #  print "XX"
+
+while 1:
+    show_depth()
